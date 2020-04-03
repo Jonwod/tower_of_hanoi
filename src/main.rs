4db594def38aec840333c43027339b1ff8500579
Jonathan Wood <jonathan.m.wood95@gmail.com>
2020-04-03 21:53:54 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,53 @@
+mod towers;
+use towers::Towers;
+
+
+fn input_usize(prompt: &str) -> usize {
+    loop {
+        println!("{}", prompt);
+        let mut buffer = String::new();
+        std::io::stdin().read_line(&mut buffer).expect("Failed to read_line");
+        let parsed = buffer.trim().parse::<usize>();
+        if let Ok(x) = parsed {
+            break x;
+        } else {
+            println!("invalid input");
+        }
+    }
+}
+
+
+// Returns a tuple representing a move (the tower
+// to take the disk from and the tower to place it on
+// respectively)
+fn get_player_move() -> (usize, usize) {
+    let source = input_usize("Enter source tower (0-2)");
+    let target = input_usize("Enter target tower (0-2)");
+    (source, target)
+}
+
+
+
+fn run_game() {
+    let mut towers = Towers::new(3);
+    while !towers.win_state() {
+        println!("\n{}", towers);
+        let (source, target) = get_player_move();
+        if !towers.mov(source, target) {
+            println!("invalid move. sorry");
+        }
+    }
+    println!("{}", towers);
+    println!("you win. well done");
+}
+
+
+fn auto_solve() {
+    let mut towers = Towers::new(3);
+
+}
+
+
+fn main() {
+    run_game();
+}
